d3f6e06a508c25a4dd4cc9ae014b14c06b3684de
komalarao <manoharvakamullu1999@gmail.com>
2022-11-30 08:38:41 -0800

    add: welcome message

---
+++ README.md
@@ -0,0 +1,2 @@
+# StackAndQueue-java
+# StackAndQueue-java
---
+++ src/main/java/com/bridgelabz/stacksandque/App.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.stacksandque;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to creating stack program!" );
+    }
+}
